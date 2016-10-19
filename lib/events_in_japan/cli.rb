e69f4c9c175925c509fc39896ad05d20ed8ec1d9
auranbuckles <auran59@gmail.com>
2016-10-19 02:40:50 -0400

    modified cli behavior

--- lib/events_in_japan/cli.rb
+++ lib/events_in_japan/cli.rb
@@ -1,17 +1,17 @@
 class EventsInJapan::CLI
 
 	def run
-		show_events
+		list_events
 		choose_event
 	end
 
-	def show_events
-		puts "Upcoming events in Japan:"
+	def list_events
+		puts "Upcoming events this month:"
+		EventsInJapan::Event.list_events
 	end
 
 	def choose_event
 		puts "Which event would you like more info on?"
-		input = gets.strip
 	end
 
 end
