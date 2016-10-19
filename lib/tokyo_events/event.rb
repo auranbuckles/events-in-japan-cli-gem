9e7c5b420d327c5f55c4e5e5d9daa7bfdcd0399f
auranbuckles <auran59@gmail.com>
2016-10-19 16:30:37 -0400

    created event initialize method

--- lib/tokyo_events/event.rb
+++ lib/tokyo_events/event.rb
@@ -7,31 +7,28 @@ class TokyoEvents::Event
 
 	@@all = []
 
+	def initialize(name, dates, url)
+		@name = name
+		@dates = dates
+		@url = url
+
+		@@all << self
+	end
+
+	def self.new_from_index_page(event)
+		self.new(
+			event.css("a").text,
+			event.css(".box_detail_ttl p").text,
+			"https://www.gotokyo.org" + event.css("a").attribute("href").value
+		)
+	end
+
 	def self.all
 		@@all
 	end
 
 	def doc
 		@doc ||= Nokogiri::HTML(open(self.url))
-	end
-
-	def self.scrape_and_create_events
-		doc = Nokogiri::HTML(open("https://www.gotokyo.org/eventlist/en/list"))
-
-		doc.css(".box_detail").each do |event|
-			event_url = "https://www.gotokyo.org" + event.css("a").attribute("href").value
-			event_doc = Nokogiri::HTML(open(event_url))
-			location_string = event_doc.css("#tmp_info_detail tr:first-child td").text
-			event_info = {
-				name: event.css("a").text,
-				location: location_string.gsub("\n\n", "").gsub("\n", ", ").gsub(/,,|,\s,/, ","),
-				description: event_doc.css(".box_photo + p").text,
-				url: "https://www.gotokyo.org" + event.css("a").attribute("href").value
-			}
-
-			events << event_info
-		end
-		events
 	end
 
 end
