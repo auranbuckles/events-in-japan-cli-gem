9335c6ebbaa7a43b6d365619cb7052a0b2b43999
auranbuckles <auran59@gmail.com>
2016-10-19 13:35:35 -0400

    used new url for scraping

--- lib/events_in_japan/event.rb
+++ lib/events_in_japan/event.rb
@@ -3,32 +3,30 @@ require 'pry'
 
 class EventsInJapan::Event
 
+	attr_accessor :name, :dates, :location, :description, :url
+
 	def self.scrape_events
-		doc = Nokogiri::HTML(open("http://www.jnto.go.jp/special_events/eng/index.php?month=10&year=2016&category=200"))
+		doc = Nokogiri::HTML(open("https://www.gotokyo.org/eventlist/en/list"))
 
-		events = []
-		doc.css("tr").each_with_index do |event, index|
-			if index != 0
-				event.css("td").each_with_index do |info, index|
-					if index == 1
-						events[:name] = info.css("strong").text
-						if info.text.match(/\d+\/\d+\/\d+/)
-							start_end_dates = info.text.scan(/\d+\/\d+\/\d+/)
-							events[:dates] = start_end_dates.join(" - ")
-						elsif info.text.match(/January|February|March|April|May|June|July|August|September|October|November|December/)
-							start_end_dates = info.text.scan(/January|February|March|April|May\s\d+.{2}|June|July|August|September|October\s\d+.{2}|November|December\s\d+.{2}/)
-							events[:dates] = start_end_dates.join(" - ")
-						end
-						events[:description] = info.text.strip
-						events[:url] = info.css("a").attribute("href").value
-					elsif index == 2
-						events[:location] = info.css("a").text.gsub(/\s+/, "")
-					end
-				end
-			end
+		doc.css(".box_detail").map do |event|
+
+			event.name = event.css("a").text
+			event_url = "https://www.gotokyo.org" + event.css("a").attribute("href").value
+
+			
+			event_doc = Nokogiri::HTML(open(event_url))
+			event_doc.css("#tmp_wrap_main p")
+
+		binding.pry
 		end
+	end
 
-		events
+	def list_events
+		
 	end
 
 end
+
+# event.dates = event.css(".box_detail_ttl p").text
+
+
