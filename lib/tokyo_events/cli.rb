8e1c52f06ab820e3d8add9aee275214ed377ec29
auranbuckles <auran59@gmail.com>
2016-10-19 15:34:41 -0400

    added CLI behavior

--- lib/tokyo_events/cli.rb
+++ lib/tokyo_events/cli.rb
@@ -6,12 +6,24 @@ class TokyoEvents::CLI
 	end
 
 	def list_events
-		puts "Upcoming events this month:"
-		TokyoEvents::Event.list_events
+		puts "============ Upcoming events this month ============"
+		TokyoEvents::Event.all.each.with_index(1) do |event, i|
+			puts "#{i}. #{event.name} - #{event.dates}"
+		end
 	end
 
 	def choose_event
 		puts "Which event would you like more info on?"
+	end
+
+	def show_event(event)
+		puts "------------ #{event.name} ------------"
+		puts ""
+		puts "Date(s): #{event.dates}"
+		puts "Location: #{event.location}"
+		puts "Description: #{event.description}"
+		puts "Link: #{event.url}"
+		puts ""
 	end
 
 	def goodbye
