f8d7c95b57ab4e21bf92967e0c7002acaaf2d9ab
auranbuckles <auran59@gmail.com>
2016-10-19 16:10:15 -0400

    moved scraper method to scraper class

--- lib/tokyo_events/event.rb
+++ lib/tokyo_events/event.rb
@@ -34,25 +34,5 @@ class TokyoEvents::Event
 		events
 	end
 
-	# def self.scrape_events
-	# 	doc = Nokogiri::HTML(open("https://www.gotokyo.org/eventlist/en/list"))
-
-	# 	events = []
-	# 	doc.css(".box_detail").each do |event|
-	# 		event_url = "https://www.gotokyo.org" + event.css("a").attribute("href").value
-	# 		event_doc = Nokogiri::HTML(open(event_url))
-	# 		location_string = event_doc.css("#tmp_info_detail tr:first-child td").text
-	# 		event_info = {
-	# 			name: event.css("a").text,
-	# 			location: location_string.gsub("\n\n", "").gsub("\n", ", ").gsub(/,,|,\s,/, ","),
-	# 			description: event_doc.css(".box_photo + p").text,
-	# 			url: "https://www.gotokyo.org" + event.css("a").attribute("href").value
-	# 		}
-
-	# 		events << event_info
-	# 	end
-	# 	events
-	# end
-
 end
 
