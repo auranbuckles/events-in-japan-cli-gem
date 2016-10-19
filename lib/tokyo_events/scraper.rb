4b7c277c4570300d6ba0c6c84eee7dfa93556d8b
auranbuckles <auran59@gmail.com>
2016-10-19 16:38:29 -0400

    removed unnecessary code

--- lib/tokyo_events/scraper.rb
+++ lib/tokyo_events/scraper.rb
@@ -8,19 +8,6 @@ class TokyoEvents::Scraper
 
 		doc.css(".box_detail").each do |event|
 			TokyoEvents::Event.new_from_index_page(event)
-
-			# event_url = "https://www.gotokyo.org" + event.css("a").attribute("href").value
-			# event_doc = Nokogiri::HTML(open(event_url))
-			# location_string = event_doc.css("#tmp_info_detail tr:first-child td").text
-			# event_info = {
-			# 	name: event.css("a").text,
-			#   dates: event.css(".box_detail_ttl p").text,
-			# 	location: location_string.gsub("\n\n", "").gsub("\n", ", ").gsub(/,,|,\s,/, ","),
-			# 	description: event_doc.css(".box_photo + p").text,
-			# 	url: "https://www.gotokyo.org" + event.css("a").attribute("href").value
-			# }
-
-			# events << event_info
 		end
 	end
 end
